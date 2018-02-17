95ba2eca17171b57f08eb6d4c04d84e690c5589e
Will <willzjc@gmail.com>
2018-02-17 19:48:15 +1100

    more CPI

--- python/historical_price/collatedata.py
+++ python/historical_price/collatedata.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
-
+import datetime
 import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
@@ -21,29 +21,87 @@ else:
 
 def drawdf(df):
 
+
+    #Normalize DF First
+
+    df['combined']=0
+    for eachitem in df.columns:
+        if not 'date' in eachitem:
+            df[eachitem] = (df[eachitem] - df[eachitem].mean()) / (df[eachitem].std())
+            # df[eachitem]= df[eachitem] * 1.5
+
+            df['combined']=df['combined']+df[eachitem]
+
+    # Get Total Rating based on average of
+    df['avg']=df[[c for c in df.columns[1:]]].mean(axis=1)
+
     # df=df.replace('\sUSD','',regex=True).apply(pd.to_numeric, errors='ignore')
     # df['dateindex']=pd.to_datetime(df['date'])
     df['date']=pd.to_datetime(df['date'])
     df=df.set_index(pd.DatetimeIndex(df['date']))
 
-    # xnew = np.linspace(df['total'].min(), df['total'].max(), 300)  # 300 represents number of points to make between T.min and T.max
 
-    # power_smooth = spline(df['date'], df['total'], xnew)
+    cpi=pd.read_csv('ref/AUCPI',delimiter='\t')
+    cpi['date']=pd.to_datetime(cpi['date'])
+
+    # Timeshift
+    timeshift=31+62
+    cpi['date'] = cpi['date'] - datetime.timedelta(days=(timeshift))
+
+    cpi=cpi.set_index(pd.DatetimeIndex(cpi['date']))
+
+    print cpi
+
+    # Normalize CPI
+    # index = pd.date_range(cpi['date'].max(),cpi['date'].min())
+    # index = pd.date_range(cpi['date'])
+    # values = pd.Series(cpi.values, index=index)
+
+    # Read above link about the different Offset Aliases, S=Seconds
+    # resampled_values = values.resample('2.5D')
+    # cpi.diff()  # compute the difference between each point!
+
+
+    cpi['CPI'] = (cpi['CPI'] - cpi['CPI'].mean()) / (cpi['CPI'].std())
+
+
+
+
+
+
+    # xnew = np.linspace(df['combined'].min(), df['combined'].max(), 300)  # 300 represents number of points to make between T.min and T.max
+
+    # power_smooth = spline(df['date'], df['combined'], xnew)
 
     # plt.plot(xnew, power_smooth)
     # plt.show()
 
+    ##### Concat both tables #####
+    print type(df),type(cpi)
+    result=pd.concat([cpi,df],axis=1, join='inner')
+
+    ##### Massaging Data #####
+    cpi = cpi.resample('D')
     df = df.resample('M').mean()
+    df = df.resample('D')
+    result=result.resample('D')
 
+    result = result.interpolate(method='cubic')
 
+    # df.plot(kind='bar')
+    # df['bad_rate'].plot(secondary_y=True)
 
-    # # Actually Drawing
-
-    tsres = df.resample('D')
-    tsint = tsres.interpolate(method='cubic')
-
-    tsint.plot()
-    tsint.plot(y='total', use_index=True)
+    #### PLOTTING SECTION #####
+    # cpi=cpi.interpolate(method='cubic')
+    # tsint.plot()
+    # tsint.plot(y='combined', use_index=True)
+    # tsint.plot()
+    # ax = cpi.plot(y='CPI', use_index=True, kind='bar')
+    # tsint.plot.line(ax=ax,y='avg',use_index=True,secondary_y=True,kind='line')
+    # cpi_s.plot(ax=ax)
+    # result.plot(y='avg',kind='bar')
+    # result.plot(y='CPI',secondary_y=True)
+    result.plot(y=['avg','CPI'])
     plt.show()
 
 def readfiles():
@@ -61,11 +119,13 @@ def readfiles():
             while len(lst_subdirs) > 0:
                 lst_subdirs.pop()
                 for file in lst_files:
-                    with open(basepath+file,'rb') as f:
-                        files.append(f.read())
-                        f.close()
+                    if '.csv' in file:
+                        with open(basepath+file,'rb') as f:
+                            files.append(f.read())
+                            f.close()
 
     for f in files:
+
         headers = {}
         buffer=[]
         headermode = True
@@ -92,15 +152,8 @@ def readfiles():
 
         df[headers['Keywords']] = currentdf['Average Selling Price']
 
-    df['total']=0
-    for eachitem in df.columns:
+    # df['combined']=0
 
-
-        if not 'date' in eachitem:
-            df[eachitem] =\
-                (df[eachitem] - df[eachitem].mean()) / (df[eachitem].max() - df[eachitem].min())
-            df['total']=df[eachitem]+df['total']
-            
     return df
 
 def main():
