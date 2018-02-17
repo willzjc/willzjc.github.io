675755ccb517634e9bb74f8e3a63af1fe024c200
Will <willzjc@gmail.com>
2018-02-17 14:07:30 +1100

    inflation

--- python/historical_price/collatedata.py
+++ python/historical_price/collatedata.py
@@ -6,60 +6,106 @@ import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly.figure_factory as FF
-
+import sys
 
 import matplotlib.pyplot as plt
 
+from scipy.interpolate import spline
 
+
+
+if sys.version_info[0] < 3:
+    from StringIO import StringIO
+else:
+    from io import StringIO
 
 def drawdf(df):
 
-    df=df.replace('\sUSD','',regex=True).apply(pd.to_numeric, errors='ignore')
-    df[u'Date']=pd.to_datetime(df[u'Date'])
-    df=df.set_index(pd.DatetimeIndex(df[u'Date']))
+    # df=df.replace('\sUSD','',regex=True).apply(pd.to_numeric, errors='ignore')
+    # df['dateindex']=pd.to_datetime(df['date'])
+    df['date']=pd.to_datetime(df['date'])
+    df=df.set_index(pd.DatetimeIndex(df['date']))
+
+    # xnew = np.linspace(df['total'].min(), df['total'].max(), 300)  # 300 represents number of points to make between T.min and T.max
+
+    # power_smooth = spline(df['date'], df['total'], xnew)
+
+    # plt.plot(xnew, power_smooth)
+    # plt.show()
 
     df = df.resample('M').mean()
-    print df
-    print df.columns
-    print df[u'Average Selling Price']
-    # df=df.groupby(pd.TimeGrouper('5Min')).sum()
 
 
 
-    # print df
-    # sample_data_table = FF.create_table(df.head())
-    # py.iplot(sample_data_table, filename='sample-data-table')
-    #
-    # trace1 = go.Scatter(
-    #                     x=df.index.values, y=df[u'Total Sales'], # Data
-    #                     mode='lines', name='Total Sales' # Additional options
-    #                    )
-    # trace2 = go.Scatter(x=df.index.values, y=df[u'Average Selling Price'], mode='lines', name='avg' )
-    # trace3 = go.Scatter(x=df.index.values, y=df[u'Items Sold'], mode='lines', name='sold')
-    #
-    # layout = go.Layout(title='Plot from csv data',
-    #                    plot_bgcolor='rgb(230, 230,230)')
-    #
-    # fig = go.Figure(data=[trace2], layout=layout)
-    #
-    # # Plot data in the notebook
-    # py.iplot(fig, filename='simple-plot-from-csv')
-    # import matplotlib.pyplot as plt
+    # # Actually Drawing
 
-    # plt.figure();
-    df.plot(y='Average Selling Price',use_index=True)
+    tsres = df.resample('D')
+    tsint = tsres.interpolate(method='cubic')
+
+    tsint.plot()
+    tsint.plot(y='total', use_index=True)
     plt.show()
 
 def readfiles():
-    basepath=os.getcwd() + '\\' + ref + '\\'
-    for f1,f2,f3 in os.walk():
-        print f3
+
+    bol_recursive=False
+    df=pd.DataFrame(columns=['date'])
+
+    basepath=os.getcwd().replace('\\','/') + '/' + 'ref' + '/'
+    print basepath
+
+    files=[]
+
+    for str_dirname, lst_subdirs, lst_files in os.walk(basepath):
+        if not bol_recursive:
+            while len(lst_subdirs) > 0:
+                lst_subdirs.pop()
+                for file in lst_files:
+                    with open(basepath+file,'rb') as f:
+                        files.append(f.read())
+                        f.close()
+
+    for f in files:
+        headers = {}
+        buffer=[]
+        headermode = True
+        for line in f.split('\n'):
+            if headermode and 'Date,' in line:
+                headermode=False
+            elements=line.strip().split(',')
+            if len(elements) < 3 and len(elements) > 1:         # filter out header info
+                headers[elements[0].strip()]=elements[1].strip()
+            else:
+                if len(elements)>1 and not headermode:
+                    buffer.append(line)
+
+        # Read file stream CSV
+        currentdf = pd.read_csv(StringIO('\n'.join(buffer)))
+
+        # Replace Strings
+        currentdf = currentdf.replace('\sUSD', '', regex=True).apply(pd.to_numeric, errors='ignore')
+        try:
+            df['date'] = currentdf['Date']
+        except:
+            # TODO
+            print 'Deal with this exception another time'
+
+        df[headers['Keywords']] = currentdf['Average Selling Price']
+
+    df['total']=0
+    for eachitem in df.columns:
+
+
+        if not 'date' in eachitem:
+            df[eachitem] =\
+                (df[eachitem] - df[eachitem].mean()) / (df[eachitem].max() - df[eachitem].min())
+            df['total']=df[eachitem]+df['total']
+            
+    return df
 
 def main():
-    # df = pd.read_csv('data_dogfood.csv')
-    # df = pd.read_csv('data_milkpowder.csv')
-    # drawdf(df)
-    readfiles()
+    df=readfiles()
+    drawdf(df)
 
 if __name__ == "__main__":
     main()
